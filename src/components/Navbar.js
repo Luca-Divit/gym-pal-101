46b5d52dda0184c5b2eda28cf3d4c6ed5ac88cca
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-05-01 15:58:05 +0100

    created nabbar added structure but need style

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Little gym logo</h1>
+      <form action="#">
+        <input type="text" placeholder="What are you looking for?" />
+      </form>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
