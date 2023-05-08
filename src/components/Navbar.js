0c35ebfe9881fb49df6781f404c7f686ea8237b7
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-05-08 18:58:02 +0200

    completed skeleton navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
+import Image from 'next/image';
+import gymPal from '../../public/gym-pal-101.png';
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Little gym logo</h1>
+    <nav className='flex justify-between items-center mx-8 h-16'>
+      <Image src={gymPal} alt="Gym Pal Logo" width={40} />
       <form action="#">
-        <input type="text" placeholder="What are you looking for?" />
+        <input className='w-48 indent-1 rounded-md' type="text" placeholder="What are you looking for?" />
       </form>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
+      <ul className='flex gap-4'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
       </ul>
     </nav>
   )
