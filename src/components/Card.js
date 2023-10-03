6ede15fb94c4a09d45e71a65b7dcff7c8591f940
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-10-03 12:45:38 +0100

    Added button to muscles cards button to redirect to muscle exercises gave it some style and displayed center in the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 
 
 const Card = ({muscle}) => {
+
+  const handleClick = (id) => {
+    console.log(`TODO redirect to muscle exercises page of muscle with id ${id}`);
+  }
   return (
     <div className="muscle-card">
       <h1>{muscle.name}</h1>
+      <button className="my-button" onClick={() => handleClick(muscle.id)}>See exercises</button>
     </div>
   )
 }
